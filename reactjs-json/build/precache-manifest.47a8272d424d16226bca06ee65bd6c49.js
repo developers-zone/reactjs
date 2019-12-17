f1b6605e5827a663aec70ac83f404d51e77b173b
Developers Zone <developers-zone@users.noreply.github.com>
2019-12-17 14:29:00 +0530

    Add files via upload

---
+++ reactjs-json/build/precache-manifest.47a8272d424d16226bca06ee65bd6c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b547504e821a58f98c0b3422583a124f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc728f346245d424ec4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d5091794fe85387d19ad",
+    "url": "/static/js/2.23b1f739.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.23b1f739.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dc728f346245d424ec4",
+    "url": "/static/js/main.6645ae02.chunk.js"
+  },
+  {
+    "revision": "9d95db193ebea89647a4",
+    "url": "/static/js/runtime-main.2114e6c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ reactjs-json/src/index.js
@@ -0,0 +1,18 @@
+
+/* fetch data
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RestController from './RestController'
+
+ReactDOM.render(
+	<RestController />, 
+	document.getElementById('root')
+);
+
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RestControllerDelete from './RestControllerDelete'
+
+ReactDOM.render(<RestControllerDelete />, document.getElementById('root'));
